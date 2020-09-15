cb5d292756c4879c7845aad03caee33f65780251
Ian Brown <bigianb@gmail.com>
2020-09-15 17:37:40 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,15 @@
 #pragma once
+#include <iostream>
 #include "SDL.h"
 
 int main(int argc, char **argv)
 {
-    return 0;
+	if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	SDL_Quit();
+	return 0;
 }
 
 
