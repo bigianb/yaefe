53607a4f55ddafa72e00b0524ee7ab969cf3758b
Ian Brown <bigianb@gmail.com>
2020-09-16 23:10:57 +0100

    small changes

--- source/Window.cpp
+++ source/Window.cpp
@@ -1,1 +1,25 @@
 #include "Window.h"
+
+void Window::init()
+{
+	window = SDL_CreateWindow("Yaefe", 
+		SDL_WINDOWPOS_UNDEFINED, 
+		SDL_WINDOWPOS_UNDEFINED, 
+		800, 800,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
+}
+
+void Window::render()
+{
+
+}
+
+void Window::exit()
+{
+	if (window) {
+		SDL_DestroyWindow(window);
+		window = nullptr;
+	}
+}
+
+
--- source/Window.h
+++ source/Window.h
@@ -1,9 +1,14 @@
 #pragma once
+#include "SDL.h"
+
 class Window
 {
 public:
-	void init() {}
-	void render() {}
-	void exit() {}
+	void init();
+	void render();
+	void exit();
+
+private:
+	SDL_Window* window = nullptr;
 };
 
